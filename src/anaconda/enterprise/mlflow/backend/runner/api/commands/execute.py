f9eaadb4b7cade444fd0c490ab9b278701157891
Josh Burt <jburt@anaconda.com>
2023-03-02 14:57:26 -0800

    use a static name

--- src/anaconda/enterprise/mlflow/backend/runner/api/commands/execute.py
+++ src/anaconda/enterprise/mlflow/backend/runner/api/commands/execute.py
@@ -90,6 +90,7 @@ class ExecuteCommand(BaseModel):
 
         # 4. Invoke job entry point with needed details
         job_create_params: Dict = {
+            "name": "RunnerAPI-Worker",
             "ident": ExecuteCommand.get_project_id(),
             "command": "Worker",
             "variables": {"MANIFEST_FILE_PATH": (request_cache_path / "manifest.json").as_posix()},
