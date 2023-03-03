1daa60529a13bdf59204c101837e9949cf1b8fa2
Josh Burt <jburt@anaconda.com>
2023-03-02 16:00:28 -0800

    stubbing

--- src/anaconda/enterprise/mlflow/backend/runner/controller.py
+++ src/anaconda/enterprise/mlflow/backend/runner/controller.py
@@ -1,13 +1,15 @@
+import json
 import shlex
 import subprocess
-import json
 from typing import Dict
 
-from anaconda.enterprise.server.contracts import BaseModel
 from anaconda.enterprise.server.common.sdk import demand_env_var
+from anaconda.enterprise.server.contracts import BaseModel
 
 from .contracts.dto.launch_parameters import LaunchParameters
+from .contracts.requests.execute import ExecuteRequest
 from .contracts.types.activity import ActivityType
+from .services.worker import WorkerService
 
 
 # pylint: disable=too-few-public-methods
@@ -47,7 +49,7 @@ class AEMLFlowBackendRunnerController(BaseModel):
         if params.activity == ActivityType.SERVER:
             AEMLFlowBackendRunnerController.launch_server(params=params)
         elif params.activity == ActivityType.WORKER:
-            self.launch_worker()
+            AEMLFlowBackendRunnerController.launch_worker()
         else:
             message = f"launch type {params.activity} is not supported"
             raise ValueError(message)
@@ -58,11 +60,13 @@ class AEMLFlowBackendRunnerController(BaseModel):
         print(cmd)
         AEMLFlowBackendRunnerController._process_launch_wait(shell_out_cmd=cmd)
 
-    def launch_worker(self):
+    @staticmethod
+    def launch_worker():
         manifest_path: str = demand_env_var(name="MANIFEST_FILE_PATH")
         print(f"Launching worker with manifest: {manifest_path}")
         with open(file=manifest_path, mode="r", encoding="utf-8") as file:
             manifest_data: Dict = json.load(file)
             print(manifest_data)
-
-
+            project_path: str = manifest_data["project_path"]
+            request: ExecuteRequest = ExecuteRequest.parse_obj(manifest_data["request"])
+            WorkerService.execute(project_path=project_path, request=request)
--- src/anaconda/enterprise/mlflow/backend/runner/services/worker.py
+++ src/anaconda/enterprise/mlflow/backend/runner/services/worker.py
@@ -1,2 +1,36 @@
+import shlex
+import shutil
+import subprocess
+
+from ..contracts.requests.execute import ExecuteRequest
+
+
 class WorkerService:
     """ """
+
+    @staticmethod
+    def _process_launch_wait(cwd: str, shell_out_cmd: str) -> None:
+        """
+        Internal function for wrapping process launches [and waiting].
+
+        Parameters
+        ----------
+        shell_out_cmd: str
+            The command to be executed.
+        """
+
+        args = shlex.split(shell_out_cmd)
+
+        with subprocess.Popen(args, cwd=cwd, stdout=subprocess.PIPE) as process:
+            for line in iter(process.stdout.readline, b""):
+                print(line)
+
+    @staticmethod
+    def execute(project_path: str, request: ExecuteRequest):
+        print("Processing mlflow step")
+        WorkerService._process_launch_wait(cwd=project_path, shell_out_cmd=request.command)
+        print("Complete")
+
+        # After completion, remove cache
+        # print("Removing worker cache")
+        # shutil.rmtree(path=project_path)
